9bf6bfc578e9d9602574682fbb450546cec7ec75
shirlyyang <shirlyyang@tencent.com>
2025-09-09 11:38:03 +0800

    language

--- node-functions/koa/[[default]].js
+++ node-functions/koa/[[default]].js
@@ -1,11 +1,11 @@
 import Koa from 'koa';
 import Router from '@koa/router';
 
-// 创建 Koa 应用
+// Create Koa application
 const app = new Koa();
 const router = new Router();
 
-// 添加一些中间件
+// Add some middleware
 app.use(async (ctx, next) => {
   const start = Date.now();
   await next();
@@ -13,15 +13,14 @@ app.use(async (ctx, next) => {
   ctx.set('X-Response-Time', `${ms}ms`);
 });
 
-// 定义路由
+// Define routes
 router.get('/', async (ctx) => {
   ctx.body = { message: 'Hello from Koa on Node Functions!' };
 });
 
-
-// 使用路由中间件
+// Use router middleware
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-// 导出处理函数
+// Export handler
 export default app;
